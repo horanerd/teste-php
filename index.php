dd0c7c90bed9ee48a3af297196648f4b73bffd55
Guilherme de sosua <51466602+horanerd@users.noreply.github.com>
2022-02-16 17:05:57 -0300

    atualização
    Ajuste da documentação e otimização das funções

--- index.php
+++ index.php
@@ -1,39 +1,47 @@
-<?php
-require_once("source/config.php");
+   <?php
 
-/***
- * 
- * Recuperar dados baseado json
- * 
- */
+    require_once("source/config.php");
 
-$pdo = Conectar();
-$users = $pdo->prepare("SELECT * FROM users");
-$hobbyvsuser = $pdo->prepare("SELECT u.name, h.hobbie FROM hobbies_x_users hs JOIN users as u ON u.id = hs.idUser JOIN hobbies as h ON h.id = hs.idHobby");
+    /***
+     * 
+     * Recuperar dados baseado json
+     * 
+     */
 
-$pais = "Brasil";
-$users = $pdo->prepare("SELECT * FROM users where country = ?");
-$users->bindParam(1, $pais);
-$users->execute();
+    /*conexão tabela users */
 
-$lista = array();
-while ($u = $users->fetch(PDO::FETCH_OBJ)) {
-    $id = $u->id;
-    $name = utf8_encode($u->name);
-    $country = $u->country;
-    $age  = calcularIdade($u->birthDate);
-    $created_at = date_fmt($u->created_at);
+    $pdo = Conectar();
+    $users = $pdo->prepare("SELECT * FROM users");
+
+    $pais = "Brasil"; // Seleção de pais de pesquisa
+
+    $users = $pdo->prepare("SELECT * FROM users where country = ?"); // Busca avançada com pais
+    $users->bindParam(1, $pais);
+    $users->execute();
+    $lista = array(); // array de usuários
+
+    while ($u = $users->fetch(PDO::FETCH_OBJ)) {
+        $id = $u->id;
+        $name = utf8_encode($u->name);
+        $country = $u->country;
+        $age  = calcularIdade($u->birthDate);
+        $created_at = date_fmt($u->created_at);
 
 
+        /* Busca a tabela hobbies baseado no usuário */
+        $hobbyvsuser = $pdo->prepare("SELECT u.name, h.hobbie FROM hobbies_x_users hs JOIN users as u ON u.id = hs.idUser JOIN hobbies as h ON h.id = hs.idHobby WHERE u.id = {$u->id}");
 
-    $hobbyvsuser = $pdo->prepare("SELECT u.name, h.hobbie FROM hobbies_x_users hs JOIN users as u ON u.id = hs.idUser JOIN hobbies as h ON h.id = hs.idHobby WHERE u.id = {$u->id}");
-    $hobbies = array();
-    if ($hobbyvsuser->execute()) {
-        while ($hb = $hobbyvsuser->fetch(PDO::FETCH_OBJ)) {
-            $h = utf8_encode($hb->hobbie);
-            $hobbies[] = $h;
+        $hobbies = array(); // array de hobbies
+
+        if ($hobbyvsuser->execute()) {
+            while ($hb = $hobbyvsuser->fetch(PDO::FETCH_OBJ)) {
+                $h = utf8_encode($hb->hobbie);
+                $hobbies[] = $h;
+            }
         }
+        $dados = ["id" => $id, "name" => $name, "country" => $country, "age" => $age, "created_at" => $created_at, "hobbie" => $hobbies];
+
+        $encode = json_encode($dados); // encode para json
+
+        echo $encode;
     }
-    $dados = ["id" => $id, "name" => $name, "country" => $country, "age" => $age, "created_at" => $created_at, "hobbie" => $hobbies];
-    echo json_encode($dados);
-}
